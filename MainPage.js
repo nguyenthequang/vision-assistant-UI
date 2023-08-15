f04ec1576982fb18d1d883fd68cb87d791022bc0
tanluuuuuuu <20521591@gm.uit.edu.vn>
2023-08-15 21:14:11 +0700

    ceter text button

--- MainPage.js
+++ MainPage.js
@@ -1,10 +1,17 @@
-import React, { useState, useRef } from 'react';
-import { View, Text, Button, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
-import { Audio } from 'expo-av'; // Import Expo Audio module for sound playback
+import React, { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  Button,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+} from "react-native";
+import { Audio } from "expo-av"; // Import Expo Audio module for sound playback
 
 const MainPage = ({ navigation }) => {
   const [messages, setMessages] = useState([]);
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
   const sound = useRef(new Audio.Sound());
 
   // Function to add a message to the chat
@@ -16,10 +23,10 @@ const MainPage = ({ navigation }) => {
   // Function to play AI response
   const playResponse = async (text) => {
     try {
-      await sound.current.loadAsync(require('./your_ai_response_sound.mp3'));
+      await sound.current.loadAsync(require("./your_ai_response_sound.mp3"));
       await sound.current.playAsync();
     } catch (error) {
-      console.log('Error playing sound', error);
+      console.log("Error playing sound", error);
     }
   };
 
@@ -39,7 +46,7 @@ const MainPage = ({ navigation }) => {
     addMessage(inputText, true);
 
     // Clear input field
-    setInputText('');
+    setInputText("");
 
     // Simulate AI response after 1 second (replace with actual AI interaction)
     setTimeout(() => {
@@ -54,7 +61,10 @@ const MainPage = ({ navigation }) => {
       {/* Chat messages */}
       <View style={styles.chatContainer}>
         {messages.map((message, index) => (
-          <View key={index} style={message.isUser ? styles.userMessage : styles.aiMessage}>
+          <View
+            key={index}
+            style={message.isUser ? styles.userMessage : styles.aiMessage}
+          >
             <Text style={styles.messageText}>{message.text}</Text>
           </View>
         ))}
@@ -75,12 +85,12 @@ const MainPage = ({ navigation }) => {
 
       {/* Mic and camera buttons */}
       <View style={styles.buttonContainer}>
-        <View style={styles.buttonRow}>
-          <Button title="Mic" onPress={handleMicPress} />
-        </View>
-        <View style={styles.buttonRow}>
-          <Button title="Camera" onPress={handleCameraPress} />
-        </View>
+        <TouchableOpacity style={styles.buttonRow} onPress={handleMicPress}>
+          <Text>Mic</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonRow} onPress={handleCameraPress}>
+          <Text>Camera</Text>
+        </TouchableOpacity>
       </View>
 
       {/* Feedback buttons */}
@@ -91,8 +101,8 @@ const MainPage = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'flex-end',
+    width: "100%",
+    height: "100%",
     paddingBottom: 20,
     paddingHorizontal: 20,
   },
@@ -101,15 +111,15 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   userMessage: {
-    backgroundColor: '#DCF8C6',
-    alignSelf: 'flex-end',
+    backgroundColor: "#DCF8C6",
+    alignSelf: "flex-end",
     padding: 10,
     borderRadius: 10,
     marginBottom: 5,
   },
   aiMessage: {
-    backgroundColor: '#E4E4E4',
-    alignSelf: 'flex-start',
+    backgroundColor: "#E4E4E4",
+    alignSelf: "flex-start",
     padding: 10,
     borderRadius: 10,
     marginBottom: 5,
@@ -119,29 +129,31 @@ const styles = StyleSheet.create({
   },
   input: {
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
   },
   sendButton: {
-    alignSelf: 'flex-end',
-    backgroundColor: '#007AFF',
+    alignSelf: "flex-end",
+    backgroundColor: "#007AFF",
     paddingVertical: 10,
     paddingHorizontal: 15,
     borderRadius: 5,
   },
   sendButtonText: {
-    color: 'white',
+    color: "white",
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    height: '33%'
+    flexDirection: "row",
+    height: "33%",
   },
   buttonRow: {
     flex: 1,
+    height: "100%",
+    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#DDDDDD',
   },
 });
 
